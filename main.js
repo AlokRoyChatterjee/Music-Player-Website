c9b85e468aa037a4bf1d79eecbc8337881160a9d
ar135 <archat71@gmail.com>
2021-07-23 22:33:28 -0400

    changes

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-let now_playing = document.querySelector(".now-playing");
-let track_art = document.querySelector(".track-art");
-let track_name = document.querySelector(".track-name");
-let track_artist = document.querySelector(".track-artist");
+let songinplay = document.querySelector(".now-playing");
+let songimage = document.querySelector(".track-art");
+let nameofsong = document.querySelector(".track-name");
+let singer = document.querySelector(".track-artist");
 
-let playpause_btn = document.querySelector(".playpause-track");
+let songbutton = document.querySelector(".playpause-track");
 let next_btn = document.querySelector(".next-track");
 let prev_btn = document.querySelector(".prev-track");
 
 let seek_slider = document.querySelector(".seek_slider");
 let volume_slider = document.querySelector(".volume_slider");
-let curr_time = document.querySelector(".current-time");
-let total_duration = document.querySelector(".total-duration");
+
 
 let track_index = 0;
 let isPlaying = false;
@@ -22,20 +21,20 @@ let curr_track = document.createElement('audio');
 // Define the tracks that have to be played
 let track_list = [
   {
-    name: "Night Owl",
-    artist: "Broke For Free",
+    name: "Electric Guitar Song",
+    artist: "Singer",
     image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
   },
   {
-    name: "Enthusiast",
-    artist: "Tours",
+    name: "Energy Pop Song",
+    artist: "Singer",
     image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
   },
   {
-    name: "Shipping Lanes",
-    artist: "Chad Crouch",
+    name: "Other Song",
+    artist: "Singer",
     image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
   },
@@ -61,10 +60,10 @@ function loadTrack(track_index) {
   curr_track.src = track_list[track_index].path;
   curr_track.load();
 
-  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
-  track_name.textContent = track_list[track_index].name;
-  track_artist.textContent = track_list[track_index].artist;
-  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+  songimage.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  nameofsong.textContent = track_list[track_index].name;
+  singer.textContent = track_list[track_index].artist;
+  songinplay.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
 
   curr_track.addEventListener("ended", nextTrack);
   random_bg_color();
@@ -86,13 +85,13 @@ function playpauseTrack() {
 function playTrack() {
   curr_track.play();
   isPlaying = true;
-  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+  songbutton.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
 
 function pauseTrack() {
   curr_track.pause();
   isPlaying = false;
-  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+  songbutton.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
 }
 
 function nextTrack() {
